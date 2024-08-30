8714f4ee6c704d4d9f4e9f9103a4502131e42095
ADNAN-an <adnan.anjar2002@gmail.com>
2024-08-30 15:14:23 +0100

    cron job added

--- datacollector/src/main/java/com/snrt/datacollector/DatacollectorApplication.java
+++ datacollector/src/main/java/com/snrt/datacollector/DatacollectorApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DatacollectorApplication {
@@ -16,5 +18,13 @@ public class DatacollectorApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@RestController
+	class WakeUpController {
+		@GetMapping("/wake-up")
+		public String wakeUp() {
+			return "Server is awake!";
+		}
 	}
 }
